529bf999228fd1e8e4c30473e7601ac29180a38e
Mohamed Warsame and Trevor Fast <mohamed@>
2017-04-25 14:55:58 -0700

    run first three unit tests

---
+++ README.md
@@ -0,0 +1,19 @@
+properties of pet
+feed
+  hungry
+    feed
+    die
+  full
+sleep
+  tired
+    sleep
+    die
+  rested
+play
+  bored
+    throws poop
+  tired
+living
+sick
+healthy
+dead
---
+++ lib/tamagotchi.rb
@@ -0,0 +1,32 @@
+class Tamagotchi
+
+  define_method(:initialize) do |name, food, rest, activity|
+    @name = name
+    @food = food
+    @rest = rest
+    @activity = activity
+
+
+    define_singleton_method(:name) do
+      @name
+    end
+    define_singleton_method(:food_level) do
+      @food = 10
+    end
+    define_singleton_method(:rest_level) do
+      @rest = 10
+    end
+    define_singleton_method(:activity_level) do
+      @activity = 10
+    end
+  end
+  define_method(:is_alive?) do
+    @food > 0
+  end
+  define_method(:set_food_level) do
+    @food = 0
+  end
+  define_method(:time_passes) do
+    @food -= 1
+  end
+end
